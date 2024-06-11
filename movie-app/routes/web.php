9c0368d0a55c26bd93e5a4dc34fc20f44d228d8a
Reza <146058320+rezzaariff@users.noreply.github.com>
2024-06-11 23:14:14 +0700

    Add files via upload

--- movie-app/routes/web.php
+++ movie-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\GenreController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,9 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product', function () {
-    return view('product');
-});
+Route::get('/products', [ProductController::class, 'index']);
 
 Route::get('/movies', function () {
     return view('movies.index');
@@ -31,26 +30,9 @@ Route::get('/reviews', function () {
     return view('reviews.index');
 });
 
-Route::get('/movies', [MovieController::class, 'index']);
+Route::resource('/movies', MovieController::class);
+Route::resource('/genres', GenreController::class);
+Route::resource('/reviews', ReviewController::class);
 
-Route::get('/movies/create', [MovieController::class, 'create']);
 
-Route::post('/movies', [MovieController::class, 'store']);
 
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
-
-Route::get('/genres', [GenreController::class, 'index']);
-
-Route::get('/genres/create', [GenreController::class, 'create']);
-
-Route::post('/genres', [GenreController::class, 'store']);
-
-Route::delete('/genres/{genre}', [GenreController::class, 'destroy']);
-
-Route::get('/reviews', [ReviewController::class, 'index']);
-
-Route::get('/reviews/create', [ReviewController::class, 'create']);
-
-Route::post('/reviews', [ReviewController::class, 'store']);
-
-Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);

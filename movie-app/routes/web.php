83145fb9937313bf0ebe1de246fa1e991b7eb34b
Reza <146058320+rezzaariff@users.noreply.github.com>
2024-06-19 10:24:43 +0700

    Add files via upload

--- movie-app/routes/web.php
+++ movie-app/routes/web.php
@@ -1,38 +1,30 @@
 <?php
 
-use App\Http\Controllers\MovieController;
-use App\Http\Controllers\GenreController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/products', [ProductController::class, 'index']);
 
-Route::get('/movies', function () {
-    return view('movies.index');
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/movies', MovieController::class);
+    Route::resource('/reviews', ReviewController::class);
+    Route::resource('/genres', GenreController::class);
 });
 
-Route::get('/reviews', function () {
-    return view('reviews.index');
-});
-
-Route::resource('/movies', MovieController::class);
-Route::resource('/genres', GenreController::class);
-Route::resource('/reviews', ReviewController::class);
-
-
-
+require __DIR__.'/auth.php';
